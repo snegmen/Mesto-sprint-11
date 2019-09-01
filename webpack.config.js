814d352f249f99dc44dbc11f4892041d3cbb9ded
snegmen <che1376@mail.ru>
2019-09-02 02:47:32 +0700

    modified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,62 +3,63 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
+
+
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].min.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: { loader: "babel-loader" },
+                exclude: /node_modules/
             },
             {
                 test: /\.css$/,
-                use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] 
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=./vendor/[name].[ext]'
             },
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
                     'file-loader?name=./images/[name].[ext]',
-                  {
-                    loader: 'image-webpack-loader',
-                    options: {
-                      mozjpeg: {
-                        progressive: true,
-                        quality: 65
-                      },
-                      optipng: {
-                        enabled: false,
-                      },
-                      pngquant: {
-                        quality: '65-90',
-                        speed: 4
-                      },
-                      gifsicle: {
-                        interlaced: false,
-                      },
-                      webp: {
-                        quality: 75
-                      }
-                    }
-                  },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
                 ],
-              },
-           {
-            test: /\.(eot|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=./vendor/[name].[ext]'
-           }
+
+            }
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css',
+            filename: 'style.[contenthash].min.css'
         }),
         new HtmlWebpackPlugin({
             inject: false,
@@ -68,7 +69,7 @@ module.exports = {
         }),
         new WebpackMd5Hash(),
         new webpack.DefinePlugin({
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-        })
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+          })
     ]
-};
+}
